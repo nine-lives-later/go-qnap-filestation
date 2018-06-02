60ac599ae0cb54ad5cafdbce2980e1c73240e4bc
Felix Kollmann <fk@konsorten.de>
2018-06-02 02:37:24 +0200

    First basic version

---
+++ filestation_system.go
@@ -0,0 +1,81 @@
+package filestation
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+type UserReference struct {
+	Id          string `json:"id"`
+	DisplayName string `json:"displayName"`
+}
+
+type loginResponse struct {
+	Status     int    `json:"status,omitempty"`
+	Version    string `json:"version,omitempty"`
+	Build      string `json:"build,omitempty"`
+	SessionID  string `json:"sid,omitempty"`
+	AdminGroup int    `json:"admingroup,omitempty"`
+}
+
+// Login perform the authentication against the QNAP storage.
+func (s *FileStationSession) Login(username, password string) error {
+	var result *loginResponse
+
+	err := s.getForEntity(&result, "cgi-bin/filemanager/wfm2Login.cgi", QueryParameters{
+		"user": username,
+		"pwd":  encodePassword(password),
+	})
+	if err != nil {
+		return err
+	}
+
+	switch result.Status {
+	case 0: // password wrong
+		return fmt.Errorf("Password or Username is invalid")
+	case 1: // success
+		s.SessionID = result.SessionID
+		return nil
+	case 8: // disabled
+		return fmt.Errorf("API is disabled")
+	}
+
+	return fmt.Errorf("Unknown status code: %v", result.Status)
+}
+
+type logoutResponse struct {
+	Status  int    `json:"status,omitempty"`
+	Version string `json:"version,omitempty"`
+	Build   string `json:"build,omitempty"`
+}
+
+// Logout invalidates the session.
+func (s *FileStationSession) Logout() error {
+	// no logged-in?
+	if s.SessionID == "" {
+		return nil
+	}
+
+	var result *logoutResponse
+
+	err := s.getForEntity(&result, "cgi-bin/filemanager/wfm2Logout.cgi", QueryParameters{
+		"sid": s.SessionID,
+	})
+	if err != nil {
+		return err
+	}
+
+	switch result.Status {
+	case 1: // success
+		s.SessionID = ""
+		return nil
+	case 8: // disabled
+		return fmt.Errorf("API is disabled")
+	}
+
+	return fmt.Errorf("Unknown status code: %v", result.Status)
+}
+
+func encodePassword(pwd string) string {
+	return base64.StdEncoding.EncodeToString([]byte(pwd))
+}
---
+++ filestation_system_test.go
@@ -0,0 +1,99 @@
+package filestation
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func createTestSession(t *testing.T) *FileStationSession {
+	return createTestSessionEx(t, "", "")
+}
+
+func createTestSessionEx(t *testing.T, username, password string) *FileStationSession {
+	// retrieve api auth if undefined
+	if username == "" {
+		username = os.Getenv("QNAP_USER")
+
+		if username == "" {
+			username = "admin"
+		}
+	}
+
+	if password == "" {
+		password = os.Getenv("QNAP_PWD")
+
+		if password == "" {
+			password = "admin"
+		}
+	}
+
+	// retrieve hostname
+	host := os.Getenv("QNAP_HOSTNAME")
+
+	if host == "" {
+		host = "storage1.konsorten.net:8443"
+	}
+
+	// create the session
+	session, err := Connect(host, username, password, nil)
+
+	if err != nil {
+		t.Fatalf("Failed to connect to QNAP File Station API: %v", err)
+	}
+
+	return session
+}
+
+func TestPasswordEncode(t *testing.T) {
+	input := "admin"
+	expected := "YWRtaW4="
+
+	if encodePassword(input) != expected {
+		t.Fatal("password encoding failed")
+	}
+}
+
+func TestInvalidHost(t *testing.T) {
+	_, err := Connect("d0esn0tex1st", "inva1dus3r", "inval1dAp1K3y", nil)
+	if err == nil {
+		t.Fatal("Error expected")
+	}
+
+	if !strings.Contains(err.Error(), "no such host") {
+		t.Fatalf("Wrong error message returned: %v", err)
+	}
+}
+
+func TestConnect(t *testing.T) {
+	s := createTestSession(t)
+
+	// real logout
+	err := s.Logout()
+	if err != nil {
+		t.Fatalf("Failed to logout: %v", err)
+	}
+
+	// redundant logout
+	err = s.Logout()
+	if err != nil {
+		t.Fatalf("Failed to logout: %v", err)
+	}
+}
+
+func TestConnect_InvalidLogin(t *testing.T) {
+	host := os.Getenv("QNAP_HOSTNAME")
+
+	if host == "" {
+		host = "storage1.konsorten.net:8443"
+	}
+
+	_, err := Connect(host, "unkn0wnUs3r", "!nval1dP@ssw0rd", nil)
+	if err == nil {
+		t.Fatal("Error expected")
+	}
+
+	if !strings.Contains(err.Error(), "Password or Username is invalid") {
+		t.Fatalf("Wrong error message returned: %v", err)
+	}
+}
